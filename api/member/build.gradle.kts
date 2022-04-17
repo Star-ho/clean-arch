da510d7c107a7121bd485f450a5abd30ab9e08c3
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-04-17 23:57:41 +0900

    feat : 멤버 모듈 추가

---
+++ api/member/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.jpa")
+}
+
+group = "com.dreamStore"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":usecase:member"))
+    implementation(project(":data:member"))
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.5")
+    implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
