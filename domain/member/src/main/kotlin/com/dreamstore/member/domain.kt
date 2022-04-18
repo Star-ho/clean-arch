b67e7ab334765d537b9ea16da1b2ebc24dde3e25
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-04-18 22:25:43 +0900

    fix : 패스워드 타입 수정

--- domain/member/src/main/kotlin/com/dreamstore/member/domain.kt
+++ domain/member/src/main/kotlin/com/dreamstore/member/domain.kt
@@ -1,10 +1,8 @@
 package com.dreamstore.member
 
-import java.math.BigDecimal
-
 class Member(
     val id:Long?=null,
     val memberId:String,
-    val password:BigDecimal,
+    val password:String,
     val email:String,
 )
