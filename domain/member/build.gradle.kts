fb9a1a5a8ef1f103dd17b423572b41260169dad7
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-04-18 22:24:45 +0900

    fix : 사용하지 않는 plugin 삭제

--- domain/member/build.gradle.kts
+++ domain/member/build.gradle.kts
@@ -3,17 +3,17 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // 루트 모듈에서 plugin 버전지정 하위모듈에선 버전지정 X
 
 plugins {
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
     kotlin("jvm")
-    kotlin("plugin.spring")
-    kotlin("plugin.jpa")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
     mavenCentral()
+}
+
+dependencies{
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
 }
 
 tasks.withType<KotlinCompile> {
