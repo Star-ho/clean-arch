39d0a4d051fedc0e71a4d859c3e2edfd07006195
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-04-22 00:54:55 +0900

    fix : price 타입 변경

--- domain/common/src/main/kotlin/com/dreamstore/common/Price.kt
+++ domain/common/src/main/kotlin/com/dreamstore/common/Price.kt
@@ -1,9 +1,10 @@
 package com.dreamstore.common
 
 class Price(price: Int) {
-    val price:BigDecimal
+    var price: Int
+
     init {
         if(price < 0) throw IllegalArgumentException("가격은 0이상어야 합니다")
-        this.price = BigDecimal(price)
+        this.price = price
     }
 }
