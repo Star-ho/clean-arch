6b49fe21785c09cab4c03df5c5baba03db8d406d
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-04-28 00:21:03 +0900

    fix : 생성자 수정

--- member/domain/src/main/kotlin/com/dreamstore/member/domain.kt
+++ member/domain/src/main/kotlin/com/dreamstore/member/domain.kt
@@ -1,10 +1,7 @@
 package com.dreamstore.member
 
 class Member(
-    val id:Long?=null,
     val memberId:String,
     val password:String,
     val email:String,
-){
-    constructor(memberId: String,password: String,email: String):this(null,memberId,password,email)
-}
+)
