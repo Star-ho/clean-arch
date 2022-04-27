ffcaa4e7d13922a50ce4a50a750329a702b3df65
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-04-28 00:22:22 +0900

    fix : 멤버 ID 타입 수정

--- member/data/src/main/kotlin/com/dreamStore/member/data.kt
+++ member/data/src/main/kotlin/com/dreamStore/member/data.kt
@@ -14,9 +14,6 @@ class MemberData (
     @Column(name="EMAIL")
     val email:String="",
 ): CommonEntity(){
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id : Long=0
 
     constructor(member: Member):this(member.memberId,member.password,
                                                     member.email)
