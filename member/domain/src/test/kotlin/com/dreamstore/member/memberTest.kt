acb8221e4355f0fba5f9de9a11165e1bac40bd72
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-04-28 23:24:28 +0900

    test : id 변수 제거

--- member/domain/src/test/kotlin/com/dreamstore/member/memberTest.kt
+++ member/domain/src/test/kotlin/com/dreamstore/member/memberTest.kt
@@ -8,13 +8,11 @@ class MemberTest{
 
     @Test
     fun createMemberTest(){
-        val id = 1L
         val memberId = "name"
         val password = "pass"
         val email = "tjdgh1231@dream.com"
-        val member = Member(id,memberId,password,email)
+        val member = Member(memberId,password,email)
 
-        assertEquals(id,member.id)
         assertEquals(memberId,member.memberId)
         assertEquals(password,member.password)
         assertEquals(email,member.email)
