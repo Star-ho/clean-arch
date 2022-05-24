3267fd27e65a89b3fb90369eec78d018a9f13309
Star-ho <58648988+Star-ho@users.noreply.github.com>
2022-05-24 18:20:01 +0900

    fix : vo 테스트 추가

--- member/domain/src/test/kotlin/com/dreamstore/member/memberTest.kt
+++ member/domain/src/test/kotlin/com/dreamstore/member/memberTest.kt
@@ -1,10 +1,7 @@
 package com.dreamstore.member
 
-import io.kotest.assertions.throwables.shouldNotThrow
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import java.lang.IllegalArgumentException
 
 
 class MemberTest{
@@ -22,19 +19,5 @@ class MemberTest{
         email shouldBe member.email.value
     }
 
-    @Test
-    fun email(){
-        var email = "tjdgh1231ream.com"
-        shouldThrow<IllegalArgumentException> {
-            Email(email)
-        }
-        email = "tjdgh1231@reamcom"
-        shouldThrow<IllegalArgumentException> {
-            Email(email)
-        }
-        email = "tjdgh1231@ream.com"
-        shouldNotThrow<IllegalArgumentException> {
-            Email(email)
-        }
-    }
+
 }
